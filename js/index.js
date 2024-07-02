cd277f24679da3bdde336db2d48748a7928cb7c0
JeevanJoudi <146952539+JeevanJoudi@users.noreply.github.com>
2024-07-02 16:39:50 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,60 @@
+
+let submitButton = document.getElementById("submit-button");
+let fullnameInput= document.getElementById("fullname");
+let emailInput= document.getElementById("email");
+let messageInput= document.getElementById("message");
+
+
+
+
+
+
+function formValidate(event){
+    event.preventDefault();
+    let formData = [];
+    let errors = [];
+
+    let fullnameContent = fullnameInput.value;
+    let messageContent = messageInput.value;
+    let emailContent = emailInput.value;
+    
+    if (fullnameContent.trim() !== ""){
+        formData.fullname = fullnameContent;
+        
+    } else{
+        errors.push("Fullname is required");
+    }
+
+
+    if (emailContent.trim() !== ""){
+        let emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        if(emailRegex.test(emailContent)){
+            formData.email=emailContent;
+        }else{
+            errors.push("Invalid email address!");
+        }
+    } else{
+        errors.push("Email is required");
+    }
+
+    if ( messageContent !== ""){
+        formData.message = messageContent;
+    } else {
+        errors.push("Enter your message!");
+    }
+     
+    if (errors.length > 0){
+        console.log("Form submission failed with errors:", errors);
+        console.log(formData);
+
+    } else {
+        console.log("Form submission Successful." , formData);
+        fullnameInput.value = "";
+        emailInput.value = "";
+        messageInput.value = "";
+    }
+     
+
+} 
+
+submitButton.addEventListener("click", formValidate);
